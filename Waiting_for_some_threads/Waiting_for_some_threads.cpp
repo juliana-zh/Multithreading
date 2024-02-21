3e1074b5eff51adbbdb345a9059b3a4c2455d55c
juliana-zh <juliana-zh@yandex.ru>
2024-02-21 19:18:27 +0300

    add id info

--- Waiting_for_some_threads/Waiting_for_some_threads.cpp
+++ Waiting_for_some_threads/Waiting_for_some_threads.cpp
@@ -1,20 +1,23 @@
 #include <thread>
 #include <vector>
 #include <iostream>
+#include <sstream>  
 
-void do_work(unsigned id);
+void do_work();
 void f() {
 	std::vector<std::thread> threads;
 	for (unsigned i = 0; i < 20; ++i) {
-		threads.emplace_back(do_work, i);
+		threads.emplace_back(do_work);
 	}
 	for (auto& entry : threads) {
 		entry.join();
 	}
 }
 
-void do_work(unsigned id) {
-	std::cout << "Hello from " << id << std::endl;
+void do_work() {
+	std::stringstream ss;
+	ss << "Hello from " << std::this_thread::get_id() << std::endl;
+	std::cout << ss.str();
 }
 
 int main(int argc, char** argv) {
